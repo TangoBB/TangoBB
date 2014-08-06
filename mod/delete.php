62ca8adcd5633972c64e86d38027bb41b2e96ca6
N8boy <jannikrenfordt@gmx.de>
2014-08-06 16:14:20 +0200

    Bugfix: Complete rework of the function for deleting a post / thread

--- mod/delete.php
+++ mod/delete.php
@@ -15,8 +15,69 @@
 
       if( !empty($query) ) {
 
-          if( $query['0']['post_type'] == "1" ) {
+        // Check if the post is a starting post
+        if( $query['0']['post_type'] == "1" ) {
+        // If it is starting post delete the post and all its subposts
+            $MYSQL->where('id', $query['0']['id']);
+            if($MYSQL->delete('{prefix}forum_posts') ) {
+                $content .= $TANGO->tpl->entity(
+                    'success_notice',
+                    'content',
+                    $LANG['mod']['delete']['thread_deleted']
+                );
+            }
+            else
+            {
+                $content .= $TANGO->tpl->entity(
+                    'danger_notice',
+                    'content',
+                    'FELHER 1'.$LANG['mod']['delete']['error_deleting']
+                );
+            }
+        // Checking if there are subposts
             $MYSQL->where('origin_thread', $query['0']['id']);
+            $subs = $MYSQL->get('{prefix}forum_posts');
+            foreach($subs as $sub) {
+                $MYSQL->where('id', $sub['id']);
+                if($MYSQL->delete('{prefix}forum_posts'))
+                {
+                    $content .= $TANGO->tpl->entity(
+                        'success_notice',
+                        'content',
+                        $LANG['mod']['delete']['thread_deleted']
+                    );    
+                }
+                else
+                {
+                    $content .= $TANGO->tpl->entity(
+                        'danger_notice',
+                        'content',
+                        'FELHER 2'.$LANG['mod']['delete']['error_deleting']
+                    );
+                }
+            }
+            
+        }
+        else // if it is just a post
+        {
+            $MYSQL->where('id', $query['0']['id']);
+            if( $MYSQL->delete('{prefix}forum_posts') ) {
+              $content .= $TANGO->tpl->entity(
+                'success_notice',
+                'content',
+                $LANG['mod']['delete']['post_deleted']
+              );
+            } else {
+              $content .= $TANGO->tpl->entity(
+                'danger_notice',
+                'content',
+                'FELHER 3'.$LANG['mod']['delete']['error_deleting']
+              );
+            }   
+        }
+         /* if( $query['0']['post_type'] == "1" ) {
+            $MYSQL->where('origin_thread', $query['0']['id']);
+            
             if( $MYSQL->delete('{prefix}forum_posts') ) {
               $MYSQL->where('id', $query['0']['id']);
               if( $MYSQL->delete('{prefix}forum_posts') ) {
@@ -29,14 +90,14 @@
                 $content .= $TANGO->tpl->entity(
                   'danger_notice',
                   'content',
-                  $LANG['mod']['delete']['error_deleting']
+                  'FELHER 1'.$LANG['mod']['delete']['error_deleting']
                 );
               }
             } else {
               $content .= $TANGO->tpl->entity(
                 'danger_notice',
                 'content',
-                $LANG['mod']['delete']['error_deleting']
+                'FELHER 2'.$LANG['mod']['delete']['error_deleting']
               );
             }
           } else {
@@ -51,11 +112,11 @@
               $content .= $TANGO->tpl->entity(
                 'danger_notice',
                 'content',
-                $LANG['mod']['delete']['error_deleting']
+                'FELHER 3'.$LANG['mod']['delete']['error_deleting']
               );
             }
           }
-
+            */
       } else {
           redirect(SITE_URL);
       }
