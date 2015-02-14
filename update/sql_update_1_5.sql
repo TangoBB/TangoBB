017426b29ab9fc7d499b609e56e9a2c859a9c8a0
jtPox <me@jtpox.com>
2015-02-15 02:32:15 +0800

    Updated the SQL file.

--- update/sql_update_1_5.sql
+++ update/sql_update_1_5.sql
@@ -1,8 +1,8 @@
 /*
-    Change the iko_ prefix to your prefix!!!!
+    Change the tan_ prefix to your prefix!!!!
  */
 
-CREATE TABLE `iko_user_comments` (
+CREATE TABLE `tan_user_comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `profile_owner` int(11) NOT NULL,
   `writer` int(11) NOT NULL,
@@ -11,10 +11,17 @@ CREATE TABLE `iko_user_comments` (
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `iko_user_visitors` (
+CREATE TABLE `tan_user_visitors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `profile_owner` int(11) NOT NULL,
   `visitor` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `tan_themes` (
+  `id` int(11) NOT NULL,
+  `theme_name` varchar(255) NOT NULL,
+  `theme_version` varchar(255) NOT NULL DEFAULT '1',
+  `theme_json_data` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
