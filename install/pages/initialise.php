a3543b787637a5882077a8b39d1580197bce4860
Sean Davies <seandavies@live.com>
2015-02-17 20:36:25 +1000

    Create initialise.php

---
+++ install/pages/initialise.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Welcome to TangoBB </title>
+    <link rel="stylesheet" href="install/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="install/assets/css/install.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-default" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#install-top">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">TangoBB Installation</a>
+        </div>
+        <div class="collapse navbar-collapse" id="install-top">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="http://codetana.com/">Codetana</a></li>
+            <li><a href="https://github.com/Codetana/Iko/wiki">Documentation</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+
+          <div class="panel panel-primary" id="main_content"><div class="panel-heading">
+            TangoBB Installation
+            </div>
+            <div class="panel-body">
+              <h4>Welcome to TangoBB</h4>
+
+              <p>Press the button below to install TangoBB on this system.</p>
+              <a href="/install" class="btn btn-success btn-block">Install</a>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+      <footer class="footer text-muted">
+        TangoBB by Codetana
+      </footer>
+    </div>
+    <script src="install/assets/js/jquery.min.js"></script>
+    <script src="install/assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
